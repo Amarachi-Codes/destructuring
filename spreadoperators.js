e7c22a1d9908a666105c1d11846dcd74de58aa31
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-27 21:42:42 +0100

    understood

---
+++ spreadoperators.js
@@ -0,0 +1,5 @@
+const subjects =['maths','english','biology','accounting','agric','literature']
+const classes = ['js2a','js1b','ss2c','ss1d','js3a','ss3b']
+
+const combine =[...subjects,...classes]
+console.log(combine);
