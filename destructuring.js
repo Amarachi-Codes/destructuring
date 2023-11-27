4d6477bc0d24cbd707c684997836e0c22335a5c3
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-27 21:11:44 +0100

    updated

--- destructuring.js
+++ destructuring.js
@@ -3,3 +3,9 @@ const countries =['Canada','Nigeria','Usa','South_Africa','Burkina_faso','Italy'
 const [c1,n3,u4,s2,b6,i2]= countries
 
 console.log(u4);
+
+let states =['Imo','Enugu','Ogun','Anambra','Lagos','Cross_River','Abia','Abuja','Delta'];
+
+[i1,e2,o3,a4,l5,c6,a7,a8,d9]=states
+
+console.log(l5);
