4fd0e898966483e44bfa0e7ec51784cf4cd2e437
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-27 21:19:34 +0100

    skipping items in an array

---
+++ skipping.js
@@ -0,0 +1,7 @@
+const clubs =['redHouse','girlsBrigade','boyScout','redCross','pressclub','debatingClub','rotrary']
+
+//skipping items using destructuring
+
+const [rh,,bs,rc,,,r]=clubs
+
+console.log(rc);
