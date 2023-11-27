c01bcfb2635f28f998b03d80232ab92e39f25dc4
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-27 22:33:10 +0100

    update

---
+++ objectdestructure.js
@@ -0,0 +1,19 @@
+const student ={
+    name: 'Kelenna',
+    age: 23,
+    eyecolor: 'brown',
+} ;
+
+const{name,age,eyecolor}=student
+console.log(age);
+
+const citizen ={
+    fullname: 'Obinna_Umeh',
+    age:'58',
+    nationality:'Nigerian',
+    SOO:'Enugu'
+}
+
+const{fullname,nationality}=citizen
+
+console.log(fullname,nationality);
